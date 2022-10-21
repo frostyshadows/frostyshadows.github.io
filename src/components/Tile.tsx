820961d069f6d52aff782a6a711010e98118de41
Sherry Yuan <frostyyshadows@gmail.com>
2022-10-21 14:31:12 -0700

    Added image

--- src/components/Tile.tsx
+++ src/components/Tile.tsx
@@ -10,23 +10,26 @@ export type TileModel = {
 
 export default function Tile(model: TileModel) {
   return (
-    <div className="inline-block justify-start text-start p-8">
-      <div className="text-3xl font-serif pb-2">
-        <p>{model.title}</p>
+    <div className="inline-block justify-start text-start p-4 relative">
+      <img src="/assets/dotted-border.png" className="w-1/4" />
+      <div className="absolute top-1/4 left-1/4">
+        <div className="text-3xl font-serif pb-2">
+          <p>{model.title}</p>
+        </div>
+        <p className="w-4/5text-start pb-4">
+          {model.description}
+        </p>
+        {
+          (model.timestamp != null) ?
+            <p className="w-4/5text-start pb-4 italic">
+              {model.timestamp}
+            </p> : null
+        }
+        {
+          (model.ctaButton != null) ?
+            CtaButton(model.ctaButton) : null
+        }
       </div>
-      <p className="w-4/5text-start pb-4">
-        {model.description}
-      </p>
-      {
-        (model.timestamp != null) ?
-          <p className="w-4/5text-start pb-4 italic">
-            {model.timestamp}
-          </p> : null
-      }
-      {
-        (model.ctaButton != null) ?
-          CtaButton(model.ctaButton) : null
-      }
     </div>
   )
 }
