c00aefdcbf61d3f1c1a5ca9a3908aec7fa299445
Sherry Yuan <frostyyshadows@gmail.com>
2022-11-08 13:29:42 -0800

    minor UI fixes

--- src/components/ExternalLinkWithIcon.tsx
+++ src/components/ExternalLinkWithIcon.tsx
@@ -8,9 +8,9 @@ export type LinkWithIconModel = {
 
 export default function ExternalLinkWithIcon(model: LinkWithIconModel) {
   return (
-    <div className="inline-flex p-2 justify-start text-start underline hover:text-indigo-600">
+    <div className="inline-flex p-2 justify-start text-start">
       <img src={model.icon} className="w-6 mr-2" />
-      <a href={model.linkUrl} target="_blank" rel="noopener noreferrer">
+      <a href={model.linkUrl} target="_blank" rel="noopener noreferrer" className="underline hover:text-indigo-600">
         {model.linkText}
       </a>
     </div>
--- src/components/ListLink.tsx
+++ src/components/ListLink.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 
 export default function ListLink(linkText: string, linkUrl: string) {
   return (
-    <div className="inline-flex px-2 py-1 justify-start text-start underline hover:text-indigo-600">
-      <a href={linkUrl} target="_blank" rel="noopener noreferrer">
+    <div className="inline-flex px-2 py-1 justify-start text-start">
+      <a href={linkUrl} target="_blank" rel="noopener noreferrer" className="underline hover:text-indigo-600">
         • {linkText}
       </a>
     </div>
